630b605551099dec2f61f9b148d537720d387b8d
liangyu <liangyu722@gmail.com>
2020-01-26 16:21:33 +1100

    Add earthquake service

---
+++ app/src/main/java/com/github/liangyu/earthquakes/data/GeoNameEarthquakeService.kt
@@ -0,0 +1,17 @@
+package com.github.liangyu.earthquakes.data
+
+import com.github.liangyu.earthquakes.data.response.EarthquakeResponse
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GeoNameEarthquakeService {
+
+    @GET("earthquakesJSON")
+    fun earthquakeAsync(
+        @Query("north") northCoordinate: Double = 44.1,
+        @Query("south") southCoordinate: Double = -9.9,
+        @Query("east") eastCoordinate: Double = -22.4,
+        @Query("west") westCoordinate: Double = 55.2
+    ): Deferred<EarthquakeResponse>
+}
