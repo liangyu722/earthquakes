78816f2a76530ab0c1935cf6cad22bc2e665100f
liangyu <liangyu722@gmail.com>
2020-01-27 12:21:54 +1100

    Add method for getting viewmodel from viewmodelfactory

---
+++ app/src/main/java/com/github/liangyu/earthquakes/util/Extension.kt
@@ -0,0 +1,17 @@
+package com.github.liangyu.earthquakes.util
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+inline fun <reified VM : ViewModel> FragmentActivity.viewModelProvider(
+    provider: ViewModelProvider.Factory
+) =
+    ViewModelProvider(this, provider).get(VM::class.java)
+
+
+inline fun <reified VM : ViewModel> Fragment.viewModelProvider(
+    provider: ViewModelProvider.Factory
+) =
+    ViewModelProvider(this, provider).get(VM::class.java)
