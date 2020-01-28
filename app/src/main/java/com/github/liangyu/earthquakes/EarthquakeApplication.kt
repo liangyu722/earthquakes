a65425c58e77b311c3f2dac835a1597a90755994
liangyu <liangyu722@gmail.com>
2020-01-28 20:41:08 +1100

    Cleanup repository inject in main activity

--- app/src/main/java/com/github/liangyu/earthquakes/EarthquakeApplication.kt
+++ app/src/main/java/com/github/liangyu/earthquakes/EarthquakeApplication.kt
@@ -4,7 +4,6 @@ import com.github.liangyu.earthquakes.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
-
 open class EarthquakeApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent.factory().create(applicationContext)
--- app/src/main/java/com/github/liangyu/earthquakes/ui/MainActivity.kt
+++ app/src/main/java/com/github/liangyu/earthquakes/ui/MainActivity.kt
@@ -3,13 +3,8 @@ package com.github.liangyu.earthquakes.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.liangyu.earthquakes.R
-import com.github.liangyu.earthquakes.data.EarthquakeRepository
-import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var repository: EarthquakeRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
